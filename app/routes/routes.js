2c640315cd4473b897f5e9d773252564f859b11e
Andrew Au <iamandrewau@gmail.com>
2018-05-07 01:01:14 -0400

    added in/decrease volume and fixed script

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,24 @@
 const cmd = require('node-cmd');
 
 module.exports = function(app) {
+  app.get('/increaseVolume', (req, res) => {
+    cmd.get('osascript -e \'output volume of (get volume settings)\'', (err, data, stderr) =>{
+      cmd.get('osascript -e \'set volume output volume ' + (parseInt(data) + 5) + '\'', (err, data, stderr) =>{
+        cmd.get('osascript -e \'output volume of (get volume settings)\'', (err, data, stderr) =>{
+          res.send({err, data, stderr});
+        });
+      });
+    });
+  });
+  app.get('/reduceVolume', (req, res) => {
+    cmd.get('osascript -e \'output volume of (get volume settings)\'', (err, data, stderr) =>{
+      cmd.get('osascript -e \'set volume output volume ' + (parseInt(data) - 5) + '\'', (err, data, stderr) =>{
+        cmd.get('osascript -e \'output volume of (get volume settings)\'', (err, data, stderr) =>{
+          res.send({err, data, stderr});
+        });
+      });
+    });
+  });
   app.get('/setVolume/:vol', (req, res) => {
     const vol = Math.round(req.params.vol);
     cmd.get('osascript -e \'set volume output volume ' + vol + '\'', (err, data, stderr) =>{
